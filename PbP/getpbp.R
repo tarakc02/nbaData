ce3f6928f83e80b721d976eac595a77605232a59
Tarak <tarak.shah@gmail.com>
2014-05-29 08:40:30 -0700

    cleaned up functions, starting to parse

--- PbP/getpbp.R
+++ PbP/getpbp.R
@@ -1,86 +1,25 @@
-setwd("~/basketball statistics/nba/scrape pbp")
-library(XML)
-library(RCurl)
-library(ggplot2)
-
-
-ESPNShot <- "http://sports.espn.go.com/nba/gamepackage/data/shot?gameId="
-ESPNPbP <- "http://sports.espn.go.com/nba/playbyplay?gameId=__GAMEID__&period=0"
-gameId <- "320223025"
-
-getPbPData <- function(ESPNPbP, gameId) {
-  if (require(XML) & require(RCurl)) {
-    PbPURL <- sub("__GAMEID__", gameId, ESPNPbP)
-    PbP <- readHTMLTable(PbPURL, stringsAsFactors=FALSE)
-    
-    # it's a bit of a hack to assume that this will always work
-    PbP <- PbP[[length(PbP)]]
-    return(PbP)
-  }
-}
-
-
-getShotData <- function(ESPNShot, gameId) {
-  if (require(XML) & require(RCurl)) {
-    shotURL <- paste(ESPNShot, gameId, sep="")
-    shots <- getURL(shotURL)
-    
-    parsed_shots <- xmlParse(shots)
-    parsed_shots <- xmlToList(parsed_shots)
-    names(parsed_shots) <- sapply(parsed_shots, function(x) x["id"])
-    return(parsed_shots)
-  }
-}
-
-parseShotDesc <- function(shotDesc) {
-  sub("(Made|Miss) ([0-9]{1,2})ft (jumper|3-pointer) ([0-9]{1,2}):([0-9]{2})(.*)", "\\1;\\2;\\4;\\5", shotDesc)
-}
-
-cleanShotData <- function(shotData) {
-  df <- (do.call(rbind, shotData))
-  df <- data.frame(df, stringsAsFactors=FALSE)
-  df$x <- as.integer(df$x)
-  df$y <- as.integer(df$y)
-  df$pid <- as.factor(df$pid)
-  df$qtr <- as.integer(df$qtr)
-  df$t <- as.factor(df$t)
-  df$made <- as.logical(df$made)
-  
-  df$y_adj <- ifelse(df$t=="h", 94-df$y, df$y)
-  df$x_adj <- ifelse(df$t=="h", 50-df$x, df$x)
-  
-  parsed_desc <- parseShotDesc(df$d)
-  parsed_desc <- do.call(rbind, strsplit(parsed_desc, ";"))
-  df$made_basket <- parsed_desc[,1] == "Made"
-  df$dist <- as.integer(parsed_desc[,2])
-  df$min <- as.integer(parsed_desc[,3])
-  df$sec <- as.integer(parsed_desc[,4])
-  df$time <- df$min*60 + df$sec
-  
-  return(df)
-}
-
-getQuarter <- function(PbP) {
-  PbP[grep("(Start|End) of the ([0-9])(st|nd|rd|th) (Quarter|Overtime)", PbP[,2]),] <-
-    sub()
-}
-
-cleanPbP <- function(PbP) {
-  PbP <- PbP[grep(":", PbP$TIME),]
-  rownames(PbP) <- seq(nrow(PbP))
-  times <- do.call(rbind, strsplit(PbP$TIME, ":"))
-  PbP$seconds_left <- (as.integer(times[,1]) *60) + as.integer(times[,2])  
-  return(PbP)
-}
+setwd("~/basketball statistics/nba/nbaData/PbP")
+source("functions/get_and_clean.R")
 
 
 
 #tests:
 ## to do: add quarter to PbP, join PbP and shotData
+ESPNShot <- "http://sports.espn.go.com/nba/gamepackage/data/shot?gameId="
+ESPNPbP <- "http://sports.espn.go.com/nba/playbyplay?gameId=__GAMEID__&period=0"
+gameId <- "400489244"
 
 PbP <- getPbPData(ESPNPbP, gameId)
+PbPOlder <- getPbPData(ESPNPbP, "260214015")
 PbP_clean <- cleanPbP(PbP)
+PbPOlder_clean <- cleanPbP(PbPOlder)
 shotRaw <- getShotData(ESPNShot, gameId)
 shotDF <- cleanShotData(shotRaw)
 
+play_anon <- gsub("<.+> ", "", PbP_clean[is.na(PbP_clean$play),"play_desc"])
+play_anon <- gsub("\\s{1,}", " ", play_anon)
+play_anon <- gsub("(\\s*\\b([A-Z][a-zA-z0-9]+)\\b)+", "__NAME__", play_anon)
+play_anon <- gsub("[0-9]{1,2}-foot", "__DISTANCE__", play_anon)
+unique(play_anon)
+table(play_anon)
 ## end tests
