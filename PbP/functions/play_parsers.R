1a48ead26341e78710e763d0a062764ff2cbede7
tarakc02 <tarakc02@users.noreply.github.com>
2014-07-11 19:17:49 -0700

    just cleaning up names

--- PbP/functions/play_parsers.R
+++ PbP/functions/play_parsers.R
@@ -1,6 +1,8 @@
-require(stringr)
-parseShots <- function(playDesc) {
-    parse <- str_match(playDesc, "^(.*) (makes|misses)( ([0-9]+)\\-foot)?([^(]*)(\\((.*) assists\\))?")
+parse_shots <- function(play_desc) {
+    if (!require(stringr)) {
+        stop("please install stringr")
+    }
+    parse <- str_match(play_desc, "^(.*) (makes|misses)( ([0-9]+)\\-foot)?([^(]*)(\\((.*) assists\\))?")
     data.frame(player=str_trim(parse[,2]), 
                made=str_trim(parse[,3])=="makes", 
                distance=as.integer(parse[,5]), 
