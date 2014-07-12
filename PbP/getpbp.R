ae8d5ede4940a849cc9072219d2239997998780b
tarakc02 <tarakc02@users.noreply.github.com>
2014-07-11 21:39:38 -0700

    more tests

--- PbP/getpbp.R
+++ PbP/getpbp.R
@@ -1,30 +1,17 @@
-setwd("~/basketball statistics/nba/nbaData/PbP")
 source("functions/get_and_clean.R")
 
-
-
 #tests:
-## to do: add quarter to PbP, join PbP and shotData
-ESPNShot <- "http://sports.espn.go.com/nba/gamepackage/data/shot?gameId="
-ESPNPbP <- "http://sports.espn.go.com/nba/playbyplay?gameId=__GAMEID__&period=0"
-gameId <- "400489244"
+shot <- "http://sports.espn.go.com/nba/gamepackage/data/shot?gameId="
+pbp <- "http://sports.espn.go.com/nba/playbyplay?gameId=__GAMEID__&period=0"
+game_id <- "400489244"
 
-PbP <- getPbPData(ESPNPbP, gameId)
-PbPOlder <- getPbPData(ESPNPbP, "260214015")
-PbP_clean <- cleanPbP(PbP)
-PbPOlder_clean <- cleanPbP(PbPOlder)
-shotRaw <- getShotData(ESPNShot, gameId)
-shotDF <- cleanShotData(shotRaw)
+pbp_raw <- get_pbp(pbp, game_id)
+pbp_clean <- clean_pbp(pbp_raw)
+shots <- get_shots(shot, game_id)
+shots_clean <- clean_shots(shots)
 
+pbp2_raw <- get_pbp(pbp, "260214015")
+pbp2_clean <- clean_pbp(pbp2_raw)
+pbp3_raw <- get_pbp(pbp, "400489678")
+pbp3_clean <- clean_pbp(pbp3_raw)
 
-
-
-
-
-play_anon <- gsub("<.+> ", "", PbP_clean[is.na(PbP_clean$play),"play_desc"])
-play_anon <- gsub("\\s{1,}", " ", play_anon)
-play_anon <- gsub("(\\s*\\b([A-Z][a-zA-z0-9]+)\\b)+", "__NAME__", play_anon)
-play_anon <- gsub("[0-9]{1,2}-foot", "__DISTANCE__", play_anon)
-unique(play_anon)
-table(play_anon)
-## end tests
