3aaff2f04ef0fadc65ef4248728a890789ad6e40
tarakc02 <tarakc02@users.noreply.github.com>
2014-07-11 21:54:14 -0700

    changed factor output to character

--- PbP/functions/play_parsers.R
+++ PbP/functions/play_parsers.R
@@ -3,11 +3,12 @@ parse_shots <- function(play_desc) {
         stop("please install stringr")
     }
     parse <- str_match(play_desc, "^(.*) (makes|misses)( ([0-9]+)\\-foot)?([^(]*)(\\((.*) assists\\))?")
-    data.frame(player   = str_trim(parse[,2]), 
-               made     = str_trim(parse[,3])=="makes", 
-               distance = as.integer(parse[,5]), 
-               type = str_trim(parse[,6]), 
-               assist   = str_trim(parse[,8]))                   
+    data.frame(player    = str_trim(parse[,2]), 
+               made      = str_trim(parse[,3])=="makes", 
+               distance  = as.integer(parse[,5]), 
+               type      = str_trim(parse[,6]), 
+               assist    = str_trim(parse[,8]),
+               stringsAsFactors = FALSE)                   
 }
 
 parse_rebounds <- function(play_desc) {
@@ -15,9 +16,10 @@ parse_rebounds <- function(play_desc) {
         stop("please install stringr")
     }
     parse <- str_match(play_desc, "(.*) (offensive|defensive) (team )?rebound")
-    data.frame(player    = temp[,2],
-               type      = temp[,3],
-               team_ind  = ifelse(temp[,4] == "", "individual", "team"))
+    data.frame(player    = parse[,2],
+               type      = parse[,3],
+               team_ind  = ifelse(parse[,4] == "", "individual", "team"),
+               stringsAsFactors = FALSE)
 }
 
 parse_freethrows <- function(play_desc) {
@@ -29,5 +31,6 @@ parse_freethrows <- function(play_desc) {
                made      = parse[,3] == "makes",
                tech      = str_trim(parse[,4]) == "technical",
                shot      = ifelse(parse[,6] == "", 1, parse[,6]),
-               out_of    = ifelse(parse[,7] == "", 1, parse[,7]))
+               out_of    = ifelse(parse[,7] == "", 1, parse[,7]),
+               stringsAsFactors = FALSE)
 }
