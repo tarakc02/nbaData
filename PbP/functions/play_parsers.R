a12591f75ac6a6d121304c9aae7d30fd393cc06c
tarakc02 <tarakc02@users.noreply.github.com>
2014-07-11 21:38:54 -0700

    added more play type parsers

--- PbP/functions/play_parsers.R
+++ PbP/functions/play_parsers.R
@@ -3,9 +3,31 @@ parse_shots <- function(play_desc) {
         stop("please install stringr")
     }
     parse <- str_match(play_desc, "^(.*) (makes|misses)( ([0-9]+)\\-foot)?([^(]*)(\\((.*) assists\\))?")
-    data.frame(player=str_trim(parse[,2]), 
-               made=str_trim(parse[,3])=="makes", 
-               distance=as.integer(parse[,5]), 
-               shotType=str_trim(parse[,6]), 
-               assist=str_trim(parse[,8]))                   
+    data.frame(player   = str_trim(parse[,2]), 
+               made     = str_trim(parse[,3])=="makes", 
+               distance = as.integer(parse[,5]), 
+               type = str_trim(parse[,6]), 
+               assist   = str_trim(parse[,8]))                   
+}
+
+parse_rebounds <- function(play_desc) {
+    if (!require(stringr)) {
+        stop("please install stringr")
+    }
+    parse <- str_match(play_desc, "(.*) (offensive|defensive) (team )?rebound")
+    data.frame(player    = temp[,2],
+               type      = temp[,3],
+               team_ind  = ifelse(temp[,4] == "", "individual", "team"))
+}
+
+parse_freethrows <- function(play_desc) {
+    if (!require(stringr)) {
+        stop("please install stringr")
+    }
+    parse <- str_match(play_desc, "(.*) (makes|misses) (technical )?free throw( ([0-9]) of ([0-9]))?")
+    data.frame(player    = parse[,2],
+               made      = parse[,3] == "makes",
+               tech      = str_trim(parse[,4]) == "technical",
+               shot      = ifelse(parse[,6] == "", 1, parse[,6]),
+               out_of    = ifelse(parse[,7] == "", 1, parse[,7]))
 }
